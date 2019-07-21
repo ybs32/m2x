363d8eeaf8f2fd32edbae3f91146040eab3eff8b
yabushi <hdecogtrexi557960@gmail.com>
2019-07-21 23:21:35 +0900

    [Add] source files

---
+++ src/ffmpeg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Ffmpeg:
+
+    def __init__(self, ffmpeg_settings):
+        self.path = ffmpeg_settings['path']
+
+    def _execute(execute):
+        print('execute')
