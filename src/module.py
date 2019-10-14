b2002dae244af392ea2941240fc0a01b72432d19
yabushi <hdecogtrexi557960@gmail.com>
2019-10-14 11:56:34 +0900

    [Add] super class

---
+++ src/module.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+
+class Module(object):
+
+    def __init__(self, configs):
+        self.path = configs['path']
+
+    def _read_config(self, configs):
+        args = []
+        for key in configs.keys():
+            args.append(key)
+            args.append(str(configs[key]))
+        return args
+
+    def _execute(self, args):
+        exec = [
+            self.path
+        ]
+        exec.extend(args)
+        subprocess.run(exec, shell=True)
